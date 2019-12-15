e5639b522fd21059bfee2944776b3763032a0378
Olaoluwakitan Fasuyi <kfasuyi@gmail.com>
2019-12-15 19:29:53 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,35 +21,21 @@ WIN_COMBINATIONS = [
     first_index = combo_arr[0]
     second_index = combo_arr[1]
     third_index = combo_arr[2]
-    
-    
-    
-    win_index_1 = WIN_COMBINATIONS[0]
-    win_index_2 = WIN_COMBINATIONS[1]
-    win_index_3 = WIN_COMBINATIONS[2]
-    win_index_4 = WIN_COMBINATIONS[3]
-    win_index_5 = WIN_COMBINATIONS[4]
-    win_index_6 = WIN_COMBINATIONS[5]
-    win_index_7 = WIN_COMBINATIONS[6]
-    win_index_8 = WIN_COMBINATIONS[7]
+   
 
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
-    position_4 = board[win_index_4]
-    position_5 = board[win_index_5]
-    position_6 = board[win_index_6]
-    position_7 = board[win_index_7]
-    position_8 = board[win_index_8]
+    position_1 = board[first_index]
+    position_2 = board[second_index]
+    position_3 = board[third_index]
+   
     
-    if position_1 == position_2 && position_2 ==position_3
+    if position_1 == position_2 && position_2 ==position_3 && position_1 != " "
+      return combo_arr
 
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combinations
-    else
-      false
     end
   end
+    
+     return FALSE
+
 
   end
 
